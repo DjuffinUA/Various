9ed97a9dc9c3bfd6e777c1a344654989dee0d737
DjuffinUA <kapatsyn.ivan@gmail.com>
2020-09-23 19:57:17 +0300

    Новый проект
    Страница персонажа 23.09.20

---
+++ C#/The character/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = null, Class = null, Title = null, Characteristics = "Характеристики: ";
+            int Level = 1, Endurance = 0, Strength = 0, Agility = 0, Intellect = 0;
+
+
+            Name = Write();
+
+            Class = ClassName();
+
+            Console.WriteLine("" + Name);
+
+        }
+
+        static string Write()
+        {
+            Console.WriteLine("");
+            string Line = Console.ReadLine();
+            return Line;
+        }
+
+        static string ClassName()
+        {
+            int n = 0;
+            string Class = null;
+            Console.WriteLine("");
+            n = Console.ReadLine();
+            if (n == 1)
+            {
+                Class = " ";
+            }
+            return Class;
+        }
+    }
+}

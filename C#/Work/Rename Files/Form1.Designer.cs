9424b8f743b04e00ad141b38cf032850fda7d70c
DjuffinUA <kapatsyn.ivan@gmail.com>
2020-11-07 12:52:54 +0200

    новая прога + Картинки с сайта frpg

---
+++ C#/Work/Rename Files/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Rename_Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button_Dir = new System.Windows.Forms.Button();
+            this.Buton_Rename = new System.Windows.Forms.Button();
+            this.TextBox_NewName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox_ToFind = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBox_Replace = new System.Windows.Forms.TextBox();
+            this.Button_Replace = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button_Dir
+            // 
+            this.Button_Dir.Location = new System.Drawing.Point(13, 27);
+            this.Button_Dir.Margin = new System.Windows.Forms.Padding(4);
+            this.Button_Dir.Name = "Button_Dir";
+            this.Button_Dir.Size = new System.Drawing.Size(137, 32);
+            this.Button_Dir.TabIndex = 0;
+            this.Button_Dir.Text = "Выбор папки";
+            this.Button_Dir.UseVisualStyleBackColor = true;
+            this.Button_Dir.Click += new System.EventHandler(this.Button_Dir_Click);
+            // 
+            // Buton_Rename
+            // 
+            this.Buton_Rename.Location = new System.Drawing.Point(478, 27);
+            this.Buton_Rename.Margin = new System.Windows.Forms.Padding(4);
+            this.Buton_Rename.Name = "Buton_Rename";
+            this.Buton_Rename.Size = new System.Drawing.Size(137, 32);
+            this.Buton_Rename.TabIndex = 1;
+            this.Buton_Rename.Text = "Переименовать";
+            this.Buton_Rename.UseVisualStyleBackColor = true;
+            this.Buton_Rename.Click += new System.EventHandler(this.Buton_Rename_Click);
+            // 
+            // TextBox_NewName
+            // 
+            this.TextBox_NewName.Location = new System.Drawing.Point(195, 31);
+            this.TextBox_NewName.Margin = new System.Windows.Forms.Padding(4);
+            this.TextBox_NewName.Name = "TextBox_NewName";
+            this.TextBox_NewName.Size = new System.Drawing.Size(232, 26);
+            this.TextBox_NewName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Найти:";
+            // 
+            // TextBox_ToFind
+            // 
+            this.TextBox_ToFind.Location = new System.Drawing.Point(195, 64);
+            this.TextBox_ToFind.Name = "TextBox_ToFind";
+            this.TextBox_ToFind.Size = new System.Drawing.Size(232, 26);
+            this.TextBox_ToFind.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Заменить на:";
+            // 
+            // TextBox_Replace
+            // 
+            this.TextBox_Replace.Location = new System.Drawing.Point(195, 96);
+            this.TextBox_Replace.Name = "TextBox_Replace";
+            this.TextBox_Replace.Size = new System.Drawing.Size(232, 26);
+            this.TextBox_Replace.TabIndex = 6;
+            // 
+            // Button_Replace
+            // 
+            this.Button_Replace.Location = new System.Drawing.Point(478, 64);
+            this.Button_Replace.Name = "Button_Replace";
+            this.Button_Replace.Size = new System.Drawing.Size(137, 58);
+            this.Button_Replace.TabIndex = 7;
+            this.Button_Replace.Text = "Заменить";
+            this.Button_Replace.UseVisualStyleBackColor = true;
+            this.Button_Replace.Click += new System.EventHandler(this.Button_Replace_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Что добавить перед именем файла:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 127);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Button_Replace);
+            this.Controls.Add(this.TextBox_Replace);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBox_ToFind);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox_NewName);
+            this.Controls.Add(this.Buton_Rename);
+            this.Controls.Add(this.Button_Dir);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Переименование файлов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_Dir;
+        private System.Windows.Forms.Button Buton_Rename;
+        private System.Windows.Forms.TextBox TextBox_NewName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBox_ToFind;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBox_Replace;
+        private System.Windows.Forms.Button Button_Replace;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ C#/Work/Rename Files/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rename_Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Dir = null;
+        
+        private void Button_Dir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.ShowNewFolderButton = false;
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                Dir =  FBD.SelectedPath;
+            }
+        }
+
+        private void Buton_Rename_Click(object sender, EventArgs e)
+        {
+            IEnumerable<FileInfo> filesToRename = Directory.GetFiles(Dir).Select(f => new FileInfo(f));
+            foreach (FileInfo file in filesToRename)
+            {
+                string newFileName = $@"{TextBox_NewName.Text}{Path.GetFileNameWithoutExtension(file.Name)}{file.Extension}";
+                string newFileFullPath = Path.Combine(file.DirectoryName, newFileName);
+                File.Move(file.FullName, newFileFullPath);
+            }
+            MessageBox.Show("Готово!");
+        }
+
+        private void Button_Replace_Click(object sender, EventArgs e)
+        {
+            IEnumerable<FileInfo> filesToRename = Directory.GetFiles(Dir).Select(f => new FileInfo(f));
+            foreach (FileInfo file in filesToRename)
+            {
+                if (TextBox_ToFind.Text == Path.GetFileNameWithoutExtension(file.Name))
+                {
+                    string newFileName = $@"{TextBox_Replace.Text}{file.Extension}";
+                    string newFileFullPath = Path.Combine(file.DirectoryName, newFileName);
+                    File.Move(file.FullName, newFileFullPath);
+                }
+            }
+        }
+    }
+}
